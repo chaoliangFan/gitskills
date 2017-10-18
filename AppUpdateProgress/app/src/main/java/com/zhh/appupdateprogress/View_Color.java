69a50c0f49060b6054fdbabafb71f002b04f7582
fanxh <2295471337@qq.com>
2017-10-18 19:49:44 +0800

    参考

---
+++ AppUpdateProgress/app/src/main/java/com/zhh/appupdateprogress/View_Color.java
@@ -0,0 +1,13 @@
+package com.zhh.appupdateprogress;
+
+import android.graphics.Color;
+
+/**
+ * Created by fanxh on 2017/10/18.
+ */
+
+public class View_Color {
+    int color = Color.BLACK;
+    int color1 = Color.argb(127,255,0,0);
+    int color2 = 0xaaff0000;
+}
