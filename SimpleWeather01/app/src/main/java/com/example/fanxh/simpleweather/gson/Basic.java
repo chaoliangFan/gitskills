12145810f09f2b43262c7d13aa88485e81327cb9
fanxh <2295471337@qq.com>
2017-11-03 13:10:55 +0800

    地区choose使用ListView，出现显示不全

---
+++ SimpleWeather01/app/src/main/java/com/example/fanxh/simpleweather/gson/Basic.java
@@ -0,0 +1,15 @@
+package com.example.fanxh.simpleweather.gson;
+
+/**
+ * Created by fanxh on 2017/10/26.
+ */
+
+public class Basic {
+    public String city;
+    public String id;
+    public Update update;
+
+    public class Update{
+        public String loc;
+    }
+}
---
+++ SimpleWeather01/app/src/main/java/com/example/fanxh/simpleweather/util/Utility.java
@@ -0,0 +1,139 @@
+package com.example.fanxh.simpleweather.util;
+
+import android.annotation.TargetApi;
+import android.graphics.Paint;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
+import android.os.Build;
+import android.text.TextUtils;
+
+import com.example.fanxh.simpleweather.db.City;
+import com.example.fanxh.simpleweather.db.County;
+import com.example.fanxh.simpleweather.db.Province;
+import com.example.fanxh.simpleweather.gson.Weather;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by fanxh on 2017/10/23.
+ */
+
+public class Utility {
+    public static Weather weather;
+    /**
+     *解析和处理服务器返回的省级数据
+     */
+    public static boolean handleProvincesResponse(String response){
+        if (!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allProvinces = new JSONArray(response);
+                for (int i = 0;i<allProvinces.length();i++){
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return  false;
+    }
+
+    /**
+     * 解析和处理服务器返回的市级数据
+     */
+    public static boolean handleCitiesResponse(String response, int provinceId){
+        if (!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allCities = new JSONArray(response);
+                for (int i = 0;i<allCities.length();i++){
+                    JSONObject cityObject = allCities.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(cityObject.getString("name"));
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return  false;
+    }
+
+    /**
+     * 解析和处理服务器返回的区或县级数据
+     */
+    public static boolean handleCountiesResponse(String response, int cityId){
+        if (!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allCounties = new JSONArray(response);
+                for (int i = 0;i<allCounties.length();i++){
+                    JSONObject countyObject = allCounties.getJSONObject(i);
+                    County county = new County();
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setWeatherId(countyObject.getString("weather_id"));
+                    county.setCityId(cityId);
+                    county.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return  false;
+    }
+
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather5");
+            String weatherInformation = jsonArray.getJSONObject(0).toString();
+         return (new Gson().fromJson(weatherInformation, Weather.class));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    public static String getWeek(String pTime) {
+        String Week = "";
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar c = Calendar.getInstance();
+        try {
+            c.setTime(format.parse(pTime));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (c.get(Calendar.DAY_OF_WEEK) == 1) {
+            Week += "日";
+        }
+        if (c.get(Calendar.DAY_OF_WEEK) == 2) {
+            Week += "一";
+        }
+        if (c.get(Calendar.DAY_OF_WEEK) == 3) {
+            Week += "二";
+        }
+        if (c.get(Calendar.DAY_OF_WEEK) == 4) {
+            Week += "三";
+        }
+        if (c.get(Calendar.DAY_OF_WEEK) == 5) {
+            Week += "四";
+        }
+        if (c.get(Calendar.DAY_OF_WEEK) == 6) {
+            Week += "五";
+        }
+        if (c.get(Calendar.DAY_OF_WEEK) == 7) {
+            Week += "六";
+        }
+        return Week;
+    }
+}
